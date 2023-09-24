d44f986f3205b6fd2e6e24e1cbffffcde77d1b27
Goitseone-Themba <goitseonerozthemba@gmail.com>
2023-09-24 21:34:25 +0200

    Added Lab1 folder with 4 classes that do file input and output. Added Lab 2 folder with 4 classes that utilise arrays and arraylists in java.

--- src/Lab2/taskFour.java
+++ src/Lab2/taskFour.java
@@ -1,7 +1,22 @@
 package Lab2;
 
+import java.util.Scanner;
 public class taskFour {
-    public static void main(String[] args) {
+    public static String pally(String in)    {
 
+        StringBuilder string = new StringBuilder();
+        string.append(in);
+        string.reverse();
+
+        if(in.equals(String.valueOf(string)))   return in + " is a palindrome";
+        return in + " is not a palindrome";
+    }
+    public static void main(String[] args) {
+        Scanner snatch = new Scanner(System.in);
+
+        System.out.println("Enter a string to check if it's a palindrome");
+        String input = snatch.nextLine();
+
+        System.out.println(pally(input));
     }
 }
