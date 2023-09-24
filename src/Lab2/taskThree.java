25e074852bf3296dd512531e97cf30373ba2f02f
goitseone-themba <goitseonerozthemba@gmail.com>
2023-09-24 08:12:40 +0200

    Lab 3 added.

--- src/Lab2/taskThree.java
+++ src/Lab2/taskThree.java
@@ -2,6 +2,9 @@ package Lab2;
 
 public class taskThree {
     public static void main(String[] args) {
+        String initial = "Vehicle", added = "123";
 
+        initial = initial.concat(added);
+        System.out.println(initial);
     }
 }
